ea25d7a45599a4267f363ad7b5be9ed6d1389f3d
Mike Ball <mikedball@gmail.com>
2012-06-02 15:28:46 -0400

    Added backbone Officials code to js-main.

--- public/javascripts/sp-base.js
+++ public/javascripts/sp-base.js
@@ -14,33 +14,80 @@ sp = {
 };
 
 $(document).ready(function() {
-  var Official = Backbone.Model.extend({
-    initialize: function() {
-      console.log('Hello ' + this.firstName + ' ' + this.lastName);
+  var officials = [
+    {
+      "id": 1,
+      "lastName": "Nutter",
+      "firstName": "Michael",
+      "nickname": "",
+      "position": "mayor",
+      "positionAlias": "Mayor",
+      "party": "D",
+      "twitter": "@Michael_Nutter",
+      "facebook": "mayornutter",
+      "website": "",
+      "photoURL": ""
     },
+    {
+      "id": 2,
+      "lastName": "Squilla",
+      "firstName": "Mark",
+      "nickname": "",
+      "position": "council",
+      "positionAlias": "Councilman (1st District)",
+      "party": "D",
+      "twitter": "@marksquilla",
+      "facebook": "",
+      "website": "http://www.phila.gov/citycouncil/squilla.html",
+      "photoURL": ""
+    } 
+  ];
+
+  var Official,
+      OfficialList,
+      ListView,
+      ProfileView;
+
+  Official = Backbone.Model.extend({
   });
 
-  var OfficialList = Backbone.Collection.extend({
-    model: Official,
-    url: 'json/officials.json'
+  OfficialList = Backbone.Collection.extend({
+    model: Official
   });
 
-  var Officials = new OfficialList;
+  ProfileView = Backbone.View.extend({
+    tagName: 'article',
+    className: 'official-profile',
+    template: $('.officialTemplate').html(),
+    render: function () {
+      var tmpl = _.template(this.template);
 
-  var OfficialView = Backbone.View.extend({
-    tagName: 'li'
-  });
-
-  var AppView = Backbone.View.extend({
-    el: $("#content"),
-
-    initialize: function() {
-      console.log('app is ready to go!');
-
-      Officials.fetch();
+      this.$el.html(tmpl(this.model.toJSON()));
+      return this;
     }
-  }); 
+  });
 
-  var App = new AppView;
+  ListView = Backbone.View.extend({
+    el: $('.official-list'),
+    initialize: function () {
+      this.collection = new OfficialList(officials),
+      this.render();
+    },
 
+    render: function () {
+        var that = this;
+        _.each(this.collection.models, function (item) {
+            that.renderProfile(item);
+        }, this);
+    },
+ 
+    renderProfile: function (item) {
+      var profileView = new ProfileView({
+        model: item
+      });
+      this.$el.append(profileView.render().el);
+    }
+  });
+
+  var list = new ListView();
 });
