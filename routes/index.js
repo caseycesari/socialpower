16e267302a1903295ee2fae758acef85ba848ed2
Mike Ball <mikedball@gmail.com>
2012-06-02 21:43:35 -0400

    Fine-tuned markup and added 'SocialPower' title to index view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var db = require('../db');
 exports.index = function(req, res){
   console.log(__dirname)
   res.render('index', {
-    title: 'Express',
+    title: 'SocialPower',
     officials: fs.readFileSync(process.cwd() + '/public/json/officials.json')
   });
 };
