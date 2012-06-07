9a3378dcc7afb6d35e1c1b3afe0a3692f31ecc70
Mike Ball <mikedball@gmail.com>
2012-06-07 09:56:15 -0400

    Fine-tuned grunt.js file to include licenses and to better compress JS resources.

--- grunt.js
+++ grunt.js
@@ -1,23 +1,37 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
   grunt.initConfig({
-    concat: {
-      dist: {
-        src: ['public/javascripts/src/components.js', 'public/javascripts/contrib/underscore-min.js', 'public/javascripts/contrib/backbone-dev.js' ],
-        dest: 'public/javascripts/dist/built.js'
-      },
-      dist: {
-        src: ['public/javascripts/src/sp-base.js'],
-        dest: 'public/javascripts/dist/base.js'
-      }
+   meta: {
+      underscoreLicense: fs.readFileSync(process.cwd() + '/public/javascripts/contrib/underscore_license.txt').toString(),
+      backboneLicense: fs.readFileSync(process.cwd() + '/public/javascripts/contrib/backbone_license.txt').toString()
     },
     min: {
-      dist: {
-        src: ['public/javascripts/dist/built.js'],
-        dest: 'public/javascripts/dist/built.min.js'
+      components: {
+        src: ['public/javascripts/src/components.js'],
+        dest: 'public/javascripts/dist/components.min.js'
       },
+      underscore: {
+        src: ['public/javascripts/contrib/underscore-min.js'],
+        dest: 'public/javascripts/dist/underscore.min.js'
+      },
+      backbone: {
+        src: ['public/javascripts/contrib/backbone-dev.js'],
+        dest: 'public/javascripts/dist/backbone-dev.min.js'
+      },
+      base: {
+        src: ['public/javascripts/src/sp-base.js'],
+        dest: 'public/javascripts/dist/sp-base.min.js'
+      }
+    },
+    concat: {
       dist: {
-        src: ['public/javascripts/dist/base.js'],
-        dest: 'public/javascripts/dist/base.min.js'
+        src: ['public/javascripts/dist/components.min.js',
+          '<banner:meta.underscoreLicense>',
+          'public/javascripts/dist/underscore.min.js',
+          '<banner:meta.backboneLicense>',
+          'public/javascripts/dist/backbone-dev.min.js'],
+        dest: 'public/javascripts/dist/built.js'
       }
     }
   });
